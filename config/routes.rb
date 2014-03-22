a0179b9d5bb33aaaa3a022d17c4cd4271aba527a
landcruiser31 <landcruiser31@gmail.com>
2014-03-22 12:42:34 +0800

    changed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Railsgirls::Application.routes.draw do
   resources :comments
 
   resources :ideas
+  root :to => redirect('/ideas')
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
